86365a594aca193b18d47d3971767fa23888a36d
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-06-25 12:38:25 +0300

    Update rm-install.sh

--- scripts/rm-install.sh
+++ scripts/rm-install.sh
@@ -978,7 +978,6 @@ echo
 echo "Remnawave URL:"
 echo "https://${PANEL_DOMAIN}/auth/login?${cookies_random1}=${cookies_random2}"
 echo
-echo "Credentials:"
 echo "Username: $SUPERADMIN_USERNAME"
 echo "Password: $SUPERADMIN_PASSWORD"
 echo
