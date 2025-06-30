9d404d4bf05886edca8acdcf64bcbdaac4b4cb99
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-07-01 01:15:20 +0300

    Update main-monitoring.sh

--- scripts/main-monitoring.sh
+++ scripts/main-monitoring.sh
@@ -682,6 +682,21 @@ server {
 
     add_header Set-Cookie \$set_cookie_header;
 
+    location /api/ {
+        proxy_http_version 1.1;
+        proxy_pass http://remnawave;
+        proxy_set_header Host $host;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection $connection_upgrade;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Host $host;
+        proxy_set_header X-Forwarded-Port $server_port;
+        proxy_send_timeout 60s;
+        proxy_read_timeout 60s;
+    }
+
     location / {
         if (\$authorized = 0) {
             return 404;
